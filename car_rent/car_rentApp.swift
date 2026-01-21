64098cb559c006b3382eff51f7a21b442943ec62
Файрушин Ян <fajrusinan6@gmail.com>
2026-01-21 12:35:00 +0500

    Initial Commit

---
+++ car_rent/car_rentApp.swift
@@ -0,0 +1,17 @@
+//
+//  car_rentApp.swift
+//  car_rent
+//
+//  Created by rair on 21.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct car_rentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
